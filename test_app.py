479d9057edbf5f7eed64e80f40108f2f14fee924
Rhitwik Prakash <rhitwik.2005@example.com>
2025-07-12 00:57:52 +0530

    Cha

--- test_app.py
+++ test_app.py
@@ -1,1 +1,9 @@
-## FOR UNIT TESTING
+## FOR UNIT TESTING, WE CREATE TEST CASES HERE
+
+from app import app
+import pytest
+
+def test_home():  # For Pytest library
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello, World! This is a Flask app running in a Docker container."
